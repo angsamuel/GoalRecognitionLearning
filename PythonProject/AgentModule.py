ec263bcddd398983b1aeef05bd87d77edfb0db1f
samuelang96 <samuel.ang.prog@gmail.com>
2018-02-15 22:22:44 -0600

    agents now plot according to their target

--- PythonProject/AgentModule.py
+++ PythonProject/AgentModule.py
@@ -8,21 +8,25 @@ class Agent:
     	self.name = name
     	self.sneaky = isSneaky
 
-    def generateQTable(self, tableSize, newGamma):
-    	self.q_table = np.matrix(np.zeros([tableSize,tableSize]))
-    	self.gamma = newGamma
+    def generateQTables(self, tableSize, newGamma, targets):
+      self.q_table_dict = dict()
+      self.gamma = newGamma
+      for target in targets:
+        q_table = np.matrix(np.zeros([tableSize,tableSize]))
+        self.q_table_dict.update({target: q_table})
 
-    def update(self, current_state, action, game_matrix):
-      max_index = np.where(self.q_table[action,] == np.max(self.q_table[action,]))[1]
+
+    def update(self, current_state, action, game_matrix, target):
+      max_index = np.where(self.q_table_dict[target][action,] == np.max(self.q_table_dict[target][action,]))[1]
       if max_index.shape[0] > 1:
         max_index = int(np.random.choice(max_index, size = 1))
       else:
         max_index = int(max_index)
-      max_value = self.q_table[action, max_index]
-      self.q_table[current_state, action] = game_matrix[current_state, action] + self.gamma * max_value
+      max_value = self.q_table_dict[target][action, max_index]
+      self.q_table_dict[target][current_state, action] = game_matrix[current_state, action] + self.gamma * max_value
       #print('max_value', game_matrix[current_state, action] + self.gamma * max_value)
   
-      if (np.max(self.q_table) > 0):
-        return(np.sum(self.q_table/np.max(self.q_table)*100))
+      if (np.max(self.q_table_dict[target]) > 0):
+        return(np.sum(self.q_table_dict[target]/np.max(self.q_table_dict[target])*100))
       else:
         return (0)
--- PythonProject/GameScenarioModule.py
+++ PythonProject/GameScenarioModule.py
@@ -13,8 +13,6 @@ class GameScenario:
       self.graph = nx.Graph()
       self.graph.add_edges_from(self.edges)
       self.pos = nx.spring_layout(self.graph)
-      self.refreshGameMatrix()
-      self.count = 1
 
     def showGraph(self):
       nx.draw_networkx_nodes(self.graph,self.pos)
@@ -22,29 +20,28 @@ class GameScenario:
       nx.draw_networkx_labels(self.graph,self.pos)
       plt.show()
 
-    def refreshGameMatrix(self):
+    def refreshGameMatrix(self, target):
       self.matrix = np.matrix(np.ones(shape=(self.nodesNum, self.nodesNum)))
       self.matrix *= -1
       for edge in self.edges:
-        if edge[1] in self.targets:
+        if edge[1] == target:
           self.matrix[edge] = 100
         else:
           self.matrix[edge] = 0
 
-        if edge[0] in self.targets:
+        if edge[0] == target:
           self.matrix[edge[::-1]] = 100
         else:
           self.matrix[edge[::-1]] = 0
 
       #targets connected to each other    
       for edge in self.edges:
-        if edge[0] in self.targets and edge[1] in self.targets:
+        if edge[0] == target and edge[1] == target:
           self.matrix[edge] = 100
           self.matrix[edge[::-1]] = 100
           
       #all goals connect to themselves
-      for target in self.targets:
-        self.matrix[target, target] = 100
+      self.matrix[target, target] = 100
 
      # print (self.matrix)
 
@@ -59,44 +56,47 @@ class GameScenario:
       next_action = int(np.random.choice(available_act,1))
       return next_action
 
-    def train_agent(self, start_state, goal_state, games):
+    def train_agent(self, start_state, games):
       new_agent = Agent("Bella", False)
-      new_agent.generateQTable(self.nodesNum, .8)
+      new_agent.generateQTables(self.nodesNum, .8, self.targets)
 
-      initial_state = 1
-      possible_action = self.get_possible_actions(initial_state)
-      action = self.preview_next_action(possible_action)
-      new_agent.update(initial_state, action, self.matrix)
+      for target in self.targets:
+        self.refreshGameMatrix(target)
 
-
-      scores = []
-      for i in range(games):
-        current_state = np.random.randint(0, int(new_agent.q_table.shape[0]))
-        possible_action = self.get_possible_actions(current_state)
+        possible_action = self.get_possible_actions(start_state)
         action = self.preview_next_action(possible_action)
-        score = new_agent.update(current_state,action,self.matrix)
-        scores.append(score)
-        #print ('Score:', str(score))
-      print("Game Matrix")
-      print(self.matrix)
-      print("Final Trained Q Table:")
-      print(new_agent.q_table/np.max(new_agent.q_table)*100)
 
-      #test results
-      # Testing
-      current_state = start_state
-      steps = [current_state]
-      q_table = new_agent.q_table
-      while current_state != goal_state:
-          print(current_state)
-          next_step_index = np.where(q_table[current_state,] == np.max(q_table[current_state,]))[1]
-          if next_step_index.shape[0] > 1:
-              next_step_index = int(np.random.choice(next_step_index, size = 1))
-          else:
-              next_step_index = int(next_step_index)
-          steps.append(next_step_index)
-          current_state = next_step_index
-      print("Most efficient path:")
-      print(steps)
-      plt.plot(scores)
-      plt.show()
+        new_agent.update(start_state, action, self.matrix, target)
+        scores = []
+        for i in range(games):
+          current_state = np.random.randint(0, int(new_agent.q_table_dict[target].shape[0]))
+          possible_action = self.get_possible_actions(current_state)
+          action = self.preview_next_action(possible_action)
+          score = new_agent.update(current_state,action,self.matrix, target)
+          scores.append(score)
+          #print ('Score:', str(score))
+        print("Game Matrix")
+        print(self.matrix)
+        print("Final Trained Q Table:")
+        print(new_agent.q_table_dict[target]/np.max(new_agent.q_table_dict[target])*100)
+
+        #test results
+        # Testing
+        current_state = start_state
+        steps = [current_state]
+        q_table = new_agent.q_table_dict[target]
+        while current_state != target:
+            #print(current_state)
+            #print(target)
+            next_step_index = np.where(q_table[current_state,] == np.max(q_table[current_state,]))[1]
+            if next_step_index.shape[0] > 1:
+                next_step_index = int(np.random.choice(next_step_index, size = 1))
+            else:
+                next_step_index = int(next_step_index)
+            steps.append(next_step_index)
+            current_state = next_step_index
+        print("Most efficient path:")
+        print(steps)
+        plt.plot(scores)
+        plt.show()
+        return new_agent
